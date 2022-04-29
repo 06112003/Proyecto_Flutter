1ec1bc734f27f68f7dd3d00426b540b2cfc58b8c
06112003 <fredy12cvpu@gmail.com>
2022-04-28 21:50:34 -0500

    Desarrollo del logim

---
+++ android/app/src/main/kotlin/com/example/amigo_almacen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amigo_almacen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:amigo_almacen/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      title: 'Amigo_Almacen',
+      initialRoute: LoginPage.routeName,
+      routes: {
+        LoginPage.routeName: (_) => LoginPage(),
+      },
+    );
+  }
+}
